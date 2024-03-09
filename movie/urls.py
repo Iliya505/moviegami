71815d004826b1aa8da20b9309c8d730d180f377
Iliya505 <iliyashokri2@gmail.com>
2024-03-09 12:39:21 -0800

    list_view

---
+++ movie/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('movies/', views.MovieListView.as_view(), name='movie')
+]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DetailView
+from .models import Movie
 
 # Create your views here.
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = 'movie\movie_list.html'
+    context_object_name = 'movies'
