805f9baec9f0f819308426c9c2586294f473bfde
Iliya505 <iliyashokri2@gmail.com>
2024-03-11 09:43:19 -0700

    register

--- moviegami/urls.py
+++ moviegami/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movie.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
